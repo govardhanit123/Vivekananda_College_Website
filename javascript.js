3aa60c2085754f6349373492d365f895306306ea
govardhanit123 <govardhansiddalait@gmail.com>
2023-01-12 06:30:42 +0530

    first Website

---
+++ javascript.js
@@ -0,0 +1,29 @@
+// Js for Toggle
+var navLink = document.getElementById('navLinks');
+function showMenu(){
+    navLink.style.right = "0"
+}
+function hideMenu(){
+    navLink.style.right = "-200px"
+}
+
+// slideing
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
